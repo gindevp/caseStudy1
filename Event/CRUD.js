f86668ac3150836c9c37fe865fafc1b91c08aeaa
= <=>
2022-06-26 21:25:27 +0700

    done game

--- Event/CRUD.js
+++ Event/CRUD.js
@@ -1,14 +1,30 @@
+var anonimouId;
+function editGirl(id){
+    console.log("edit id:" +id)
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].id == id) {
+            anonimouId = arr[i].id;
+            document.getElementById("name").value = arr[i].name;
+            document.getElementById("age").value = arr[i].age;
+            document.getElementById("images").value = arr[i].images;
+            document.getElementById("addRess").value = arr[i].address;
+            document.getElementById("phone").value = arr[i].phone;
+            document.getElementById("hobbies").value = arr[i].hobbies;
+        }
+    }
+}
 function addGirl(){
-    let iteamName = document.getElementById('name').value
-    let itemAge = document.getElementById('age').value
+    let itemName = document.getElementById('name').value
+    let itemAge = Number(document.getElementById('age').value)
     let itemImages=document.getElementById("images").value
     let itemAddRess = document.getElementById('addRess').value
     let itemPhone = document.getElementById('phone').value
     let itemHobbies = document.getElementById('hobbies').value
     let item;
     item={
-        id: (arr.length +1),
-        name: iteamName ,
+        id: anonimouId,
+        name: itemName ,
         age: itemAge,
         images: itemImages,
         address: itemAddRess ,
@@ -19,9 +35,13 @@ function addGirl(){
     console.log(item.id)
     console.log("index" + index)
     if (index >= 0) {
+        console.log(" edit thanh cong index:"+index)
         arr.splice(index, 1, item)
     } else {
-        console.log(index);
+        item.id= (arr.length+1)
+        item.images= "/img/"+item.images+".jpg"
+        console.log("add thanh cong")
+        console.log("item"+item);
         arr.push(item);
     }
     console.log(arr)
@@ -37,25 +57,11 @@ function clear(){
     document.getElementById('hobbies').value=""
 }
 function deleteGirl(id){
-    console.log()
+    console.log("delete"+id)
     for (let i = 0; i < arr.length; i++) {
         if (arr[i].id == id) {
             arr.splice(i, 1);
             render();
         }
 
-
     }}
-function editGirl(id){
-    console.log("edit")
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].id == id) {
-            document.getElementById("name").value = arr[i].name;
-            document.getElementById("age").value = arr[i].age;
-            document.getElementById("images").value = arr[i].images;
-            document.getElementById("addRess").value = arr[i].address;
-            document.getElementById("phone").value = arr[i].phone;
-            document.getElementById("hobbies").value = arr[i].hobbies;
-        }
-    }
-}
